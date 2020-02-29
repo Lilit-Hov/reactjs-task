78953362ec405eac7b25df2b5d50f9de4dd7f9dc
Lilit-Hov <hovakimyanlilit.a@gmail.com>
2020-02-29 04:49:31 +0400

    last commit,code review

--- src/components/SelectBox/index.js
+++ src/components/SelectBox/index.js
@@ -2,28 +2,12 @@ import React from "react";
 import './style.scss';
 
 class SelectBox extends React.Component {
-    constructor() {
-        super();
-        this.state = {
-            value: 'placeholder'
-        }
-    }
-
-    onValueChange = (e) => {
-        this.setState({
-            value: e.target.value
-        });
-
-        this.props.onChange(e.target.value)
-    };
-
     render() {
         return(
             <div>
                 <label className="text-uppercase label-text">Select Template</label>
-                <select className="select-control" value={this.state.value}
-                        onChange={(e) => this.onValueChange(e)}>
-                    <option value="placeholder" disabled>Select template</option>
+                <select className="select-control"  onChange={(e) => this.props.onChange(e.target.value)}>
+                    <option value="placeholder" selected disabled>Select template</option>
                     {
                         this.props.items.map((item,index) => (
                             <option value={item.value} key={index}>{item.label}</option>
